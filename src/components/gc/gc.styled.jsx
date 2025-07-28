c7d4122392707ddf2d5d27a62da713f502c78a93
CRCunha <cristianraffi92@gmail.com>
2025-07-28 13:12:01 -0300

    gc

--- src/components/gc/gc.styled.jsx
+++ src/components/gc/gc.styled.jsx
@@ -1,45 +1,40 @@
-import styled, { keyframes } from 'styled-components';
-import { primary, secondary } from '../../constants/color';
+import styled from 'styled-components';
+import { secondary } from '../../constants/color';
 
 export const Container = styled.div`
-  width: 80vw;
-  height: 180px;
-
   display: flex;
   flex-direction: column;
 `;
 
 export const GcTop = styled.div`
-  width: 68%;
-  height: 50px;
-
   background-color: ${secondary};
-
-  padding-left: 20px;
-
-  display: flex;
+  padding: 10px 20px;
+  display: inline-flex; /* <-- muda para inline-flex */
   align-items: center;
 `;
 
-export const TextContainerTop = styled.textarea`
-  width: 100%;
-  height: 100%;
-
-  margin-top: 15px;
-
-  background-color: transparent;
-  border: none;
-  vertical-align: middle;
-
-  display: flex;
-  align-items: center;
-
+export const TextMirror = styled.span`
+  position: absolute;
+  visibility: hidden;
+  white-space: pre;
   font-size: 28px;
   font-weight: 600;
-
   text-transform: uppercase;
+  font-family: inherit;
+`;
 
+export const TextContainerTop = styled.textarea`
+  height: 1.2em;
+  resize: none;
+  overflow: hidden;
+  background-color: transparent;
+  border: none;
+  font-size: 28px;
+  font-weight: 600;
+  text-transform: uppercase;
   color: #fff;
+  white-space: nowrap;
+  display: inline-block; /* <-- impede ocupar 100% */
 
   &:focus {
     outline: none;
@@ -47,39 +42,30 @@ export const TextContainerTop = styled.textarea`
 `;
 
 export const GcBottom = styled.div`
-  width: 78%;
-  height: calc(80% - 50px);
-
+  width: auto;
   background-color: #f9f9f9;
-
-  padding-left: 20px;
-
-  display: flex;
+  padding: 10px 20px;
+  display: inline-flex; /* idem aqui */
   align-items: center;
+`;
 
-  color: #505050;
-
-  font-size: 22px;
+export const TextMirrorBottom = styled(TextMirror)`
+  font-size: 26px;
   font-weight: 500;
-  border: none;
-  outline: none;
-  resize: none; /* opcional: remove o controle de redimensionamento */
+  color: #505050;
 `;
 
 export const TextContainer = styled.textarea`
-  width: 100%;
-
-  margin-top: 15px;
-
+  height: 1.2em;
+  resize: none;
+  overflow: hidden;
   background-color: transparent;
   border: none;
-  vertical-align: middle;
-  color: #505050;
-
   font-size: 26px;
-  border: none;
-  outline: none;
-  resize: none;
+  color: #505050;
+  white-space: nowrap;
+  display: inline-block; /* impede ocupar largura total */
+  transition: width 0.2s ease;
 
   &:focus {
     outline: none;
--- src/components/gc/index.jsx
+++ src/components/gc/index.jsx
@@ -1,24 +1,70 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
   Container,
   GcTop,
   GcBottom,
   TextContainer,
   TextContainerTop,
+  TextMirror,
+  TextMirrorBottom,
 } from './gc.styled.jsx';
 
 const Gc = () => {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+
+  const mirrorTopRef = useRef(null);
+  const topRef = useRef(null);
+  const mirrorBottomRef = useRef(null);
+  const bottomRef = useRef(null);
+
+  // Ajusta largura pelo espelho
+  const adjustWidth = (mirrorRef, inputRef, text) => {
+    if (mirrorRef.current && inputRef.current) {
+      mirrorRef.current.textContent = text || ' ';
+      inputRef.current.style.width = mirrorRef.current.offsetWidth + 'px';
+    }
+  };
+
+  // Carrega dados do localStorage
+  useEffect(() => {
+    const savedTop = localStorage.getItem('gcTopText');
+    const savedBottom = localStorage.getItem('gcBottomText');
+
+    if (savedTop) setTopText(savedTop);
+    if (savedBottom) setBottomText(savedBottom);
+  }, []);
+
+  // Atualiza largura e salva no localStorage
+  useEffect(() => {
+    adjustWidth(mirrorTopRef, topRef, topText);
+    localStorage.setItem('gcTopText', topText);
+  }, [topText]);
+
+  useEffect(() => {
+    adjustWidth(mirrorBottomRef, bottomRef, bottomText);
+    localStorage.setItem('gcBottomText', bottomText);
+  }, [bottomText]);
+
   return (
     <Container>
       <GcTop>
-        <TextContainerTop spellcheck="false">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </TextContainerTop>
+        <TextMirror ref={mirrorTopRef} />
+        <TextContainerTop
+          ref={topRef}
+          spellCheck="false"
+          value={topText}
+          onChange={(e) => setTopText(e.target.value)}
+        />
       </GcTop>
       <GcBottom>
-        <TextContainer spellcheck="false">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </TextContainer>
+        <TextMirrorBottom ref={mirrorBottomRef} />
+        <TextContainer
+          ref={bottomRef}
+          spellCheck="false"
+          value={bottomText}
+          onChange={(e) => setBottomText(e.target.value)}
+        />
       </GcBottom>
     </Container>
   );
